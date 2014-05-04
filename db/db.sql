a13e50f4621159f5386a5a4ef3c9d71ecac145d9
marinpareja <marinpareja@gmail.com>
2014-05-04 16:44:49 +0200

    Aptana

--- db/db.sql
+++ db/db.sql
@@ -2,13 +2,13 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `test1` DEFAULT CHARACTER SET utf8 ;
-USE `test1` ;
+CREATE SCHEMA IF NOT EXISTS `sql238823` DEFAULT CHARACTER SET utf8 ;
+USE `sql238823` ;
 
 -- -----------------------------------------------------
--- Table `test1`.`Nombre`
+-- Table `sql238823`.`Nombre`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test1`.`Nombre` (
+CREATE TABLE IF NOT EXISTS `sql238823`.`Nombre` (
   `idNombre` INT NOT NULL AUTO_INCREMENT,
   `Nombre` VARCHAR(45) NULL,
   PRIMARY KEY (`idNombre`))
@@ -16,9 +16,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test1`.`Apellido`
+-- Table `sql238823`.`Apellido`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test1`.`Apellido` (
+CREATE TABLE IF NOT EXISTS `sql238823`.`Apellido` (
   `idApellido` INT NOT NULL AUTO_INCREMENT,
   `Apellido` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idApellido`))
@@ -26,9 +26,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test1`.`Persona`
+-- Table `sql238823`.`Persona`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test1`.`Persona` (
+CREATE TABLE IF NOT EXISTS `sql238823`.`Persona` (
   `idPersona` INT NOT NULL AUTO_INCREMENT,
   `Domicilio` VARCHAR(100) NULL,
   `FNacimiento` DATE NULL,
@@ -39,16 +39,16 @@ CREATE TABLE IF NOT EXISTS `test1`.`Persona` (
   INDEX `fk_Persona_Nombre1_idx` (`Nombre_idNombre` ASC),
   CONSTRAINT `fk_Persona_Nombre1`
     FOREIGN KEY (`Nombre_idNombre`)
-    REFERENCES `test1`.`Nombre` (`idNombre`)
+    REFERENCES `sql238823`.`Nombre` (`idNombre`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test1`.`Empresa`
+-- Table `sql238823`.`Empresa`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test1`.`Empresa` (
+CREATE TABLE IF NOT EXISTS `sql238823`.`Empresa` (
   `idEmpresa` INT NOT NULL,
   `Nombre` VARCHAR(45) NULL,
   `Teléfono` MEDIUMINT NULL,
@@ -59,16 +59,16 @@ CREATE TABLE IF NOT EXISTS `test1`.`Empresa` (
   INDEX `fk_Empresa_Persona1_idx` (`Persona_Contacto` ASC),
   CONSTRAINT `fk_Empresa_Persona1`
     FOREIGN KEY (`Persona_Contacto`)
-    REFERENCES `test1`.`Persona` (`idPersona`)
+    REFERENCES `sql238823`.`Persona` (`idPersona`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test1`.`Trabajador`
+-- Table `sql238823`.`Trabajador`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test1`.`Trabajador` (
+CREATE TABLE IF NOT EXISTS `sql238823`.`Trabajador` (
   `idTrabajador` INT NOT NULL,
   `Persona` INT NOT NULL,
   `Seguridad Social` BIGINT NULL,
@@ -76,16 +76,16 @@ CREATE TABLE IF NOT EXISTS `test1`.`Trabajador` (
   INDEX `fk_Trabajador_Persona1_idx` (`Persona` ASC),
   CONSTRAINT `fk_Trabajador_Persona1`
     FOREIGN KEY (`Persona`)
-    REFERENCES `test1`.`Persona` (`idPersona`)
+    REFERENCES `sql238823`.`Persona` (`idPersona`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test1`.`Servicio`
+-- Table `sql238823`.`Servicio`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test1`.`Servicio` (
+CREATE TABLE IF NOT EXISTS `sql238823`.`Servicio` (
   `idCliente` INT NOT NULL,
   `Persona_Servicio` INT NOT NULL,
   `Persona_Cliente` INT NULL,
@@ -98,31 +98,31 @@ CREATE TABLE IF NOT EXISTS `test1`.`Servicio` (
   INDEX `fk_Servicio_Trabajador1_idx` (`Trabajador` ASC),
   CONSTRAINT `fk_Cliente_Persona1`
     FOREIGN KEY (`Persona_Servicio`)
-    REFERENCES `test1`.`Persona` (`idPersona`)
+    REFERENCES `sql238823`.`Persona` (`idPersona`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Servicio_Persona1`
     FOREIGN KEY (`Persona_Cliente`)
-    REFERENCES `test1`.`Persona` (`idPersona`)
+    REFERENCES `sql238823`.`Persona` (`idPersona`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Servicio_Empresa1`
     FOREIGN KEY (`Empresa_Cliente`)
-    REFERENCES `test1`.`Empresa` (`idEmpresa`)
+    REFERENCES `sql238823`.`Empresa` (`idEmpresa`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Servicio_Trabajador1`
     FOREIGN KEY (`Trabajador`)
-    REFERENCES `test1`.`Trabajador` (`idTrabajador`)
+    REFERENCES `sql238823`.`Trabajador` (`idTrabajador`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test1`.`Articulo`
+-- Table `sql238823`.`Articulo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test1`.`Articulo` (
+CREATE TABLE IF NOT EXISTS `sql238823`.`Articulo` (
   `idArticulo` INT NOT NULL,
   `Nombre` VARCHAR(45) NOT NULL,
   `Caracteristicas` VARCHAR(140) NULL,
@@ -135,9 +135,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test1`.`Proveedor`
+-- Table `sql238823`.`Proveedor`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test1`.`Proveedor` (
+CREATE TABLE IF NOT EXISTS `sql238823`.`Proveedor` (
   `idProveedor` INT NOT NULL,
   `Empresa` INT NOT NULL,
   `Articulo` INT NOT NULL,
@@ -146,21 +146,21 @@ CREATE TABLE IF NOT EXISTS `test1`.`Proveedor` (
   INDEX `fk_Proveedor_Articulo1_idx` (`Articulo` ASC),
   CONSTRAINT `fk_Proveedor_Empresa1`
     FOREIGN KEY (`Empresa`)
-    REFERENCES `test1`.`Empresa` (`idEmpresa`)
+    REFERENCES `sql238823`.`Empresa` (`idEmpresa`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Proveedor_Articulo1`
     FOREIGN KEY (`Articulo`)
-    REFERENCES `test1`.`Articulo` (`idArticulo`)
+    REFERENCES `sql238823`.`Articulo` (`idArticulo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test1`.`Articulo_has_Servicio`
+-- Table `sql238823`.`Articulo_has_Servicio`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test1`.`Articulo_has_Servicio` (
+CREATE TABLE IF NOT EXISTS `sql238823`.`Articulo_has_Servicio` (
   `Articulo_idArticulo` INT NOT NULL,
   `Servicio_idCliente` INT NOT NULL,
   PRIMARY KEY (`Articulo_idArticulo`, `Servicio_idCliente`),
@@ -168,21 +168,21 @@ CREATE TABLE IF NOT EXISTS `test1`.`Articulo_has_Servicio` (
   INDEX `fk_Articulo_has_Servicio_Articulo1_idx` (`Articulo_idArticulo` ASC),
   CONSTRAINT `fk_Articulo_has_Servicio_Articulo1`
     FOREIGN KEY (`Articulo_idArticulo`)
-    REFERENCES `test1`.`Articulo` (`idArticulo`)
+    REFERENCES `sql238823`.`Articulo` (`idArticulo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Articulo_has_Servicio_Servicio1`
     FOREIGN KEY (`Servicio_idCliente`)
-    REFERENCES `test1`.`Servicio` (`idCliente`)
+    REFERENCES `sql238823`.`Servicio` (`idCliente`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test1`.`Forma_de_Pago`
+-- Table `sql238823`.`Forma_de_Pago`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test1`.`Forma_de_Pago` (
+CREATE TABLE IF NOT EXISTS `sql238823`.`Forma_de_Pago` (
   `idForma_de_Pago` INT NOT NULL,
   `Forma_de_Pago` VARCHAR(45) NULL,
   PRIMARY KEY (`idForma_de_Pago`))
@@ -190,9 +190,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test1`.`Proveedor_has_Forma_de_Pago`
+-- Table `sql238823`.`Proveedor_has_Forma_de_Pago`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test1`.`Proveedor_has_Forma_de_Pago` (
+CREATE TABLE IF NOT EXISTS `sql238823`.`Proveedor_has_Forma_de_Pago` (
   `Proveedor_idProveedor` INT NOT NULL,
   `Forma_de_Pago_idForma_de_Pago` INT NOT NULL,
   PRIMARY KEY (`Proveedor_idProveedor`, `Forma_de_Pago_idForma_de_Pago`),
@@ -200,21 +200,21 @@ CREATE TABLE IF NOT EXISTS `test1`.`Proveedor_has_Forma_de_Pago` (
   INDEX `fk_Proveedor_has_Forma_de_Pago_Proveedor1_idx` (`Proveedor_idProveedor` ASC),
   CONSTRAINT `fk_Proveedor_has_Forma_de_Pago_Proveedor1`
     FOREIGN KEY (`Proveedor_idProveedor`)
-    REFERENCES `test1`.`Proveedor` (`idProveedor`)
+    REFERENCES `sql238823`.`Proveedor` (`idProveedor`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Proveedor_has_Forma_de_Pago_Forma_de_Pago1`
     FOREIGN KEY (`Forma_de_Pago_idForma_de_Pago`)
-    REFERENCES `test1`.`Forma_de_Pago` (`idForma_de_Pago`)
+    REFERENCES `sql238823`.`Forma_de_Pago` (`idForma_de_Pago`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test1`.`Forma_de_Pago_has_Trabajador`
+-- Table `sql238823`.`Forma_de_Pago_has_Trabajador`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test1`.`Forma_de_Pago_has_Trabajador` (
+CREATE TABLE IF NOT EXISTS `sql238823`.`Forma_de_Pago_has_Trabajador` (
   `Forma_de_Pago_idForma_de_Pago` INT NOT NULL,
   `Trabajador_idTrabajador` INT NOT NULL,
   PRIMARY KEY (`Forma_de_Pago_idForma_de_Pago`, `Trabajador_idTrabajador`),
@@ -222,21 +222,21 @@ CREATE TABLE IF NOT EXISTS `test1`.`Forma_de_Pago_has_Trabajador` (
   INDEX `fk_Forma_de_Pago_has_Trabajador_Forma_de_Pago1_idx` (`Forma_de_Pago_idForma_de_Pago` ASC),
   CONSTRAINT `fk_Forma_de_Pago_has_Trabajador_Forma_de_Pago1`
     FOREIGN KEY (`Forma_de_Pago_idForma_de_Pago`)
-    REFERENCES `test1`.`Forma_de_Pago` (`idForma_de_Pago`)
+    REFERENCES `sql238823`.`Forma_de_Pago` (`idForma_de_Pago`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Forma_de_Pago_has_Trabajador_Trabajador1`
     FOREIGN KEY (`Trabajador_idTrabajador`)
-    REFERENCES `test1`.`Trabajador` (`idTrabajador`)
+    REFERENCES `sql238823`.`Trabajador` (`idTrabajador`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test1`.`Servicio_has_Forma_de_Pago`
+-- Table `sql238823`.`Servicio_has_Forma_de_Pago`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test1`.`Servicio_has_Forma_de_Pago` (
+CREATE TABLE IF NOT EXISTS `sql238823`.`Servicio_has_Forma_de_Pago` (
   `Servicio_idCliente` INT NOT NULL,
   `Forma_de_Pago_idForma_de_Pago` INT NOT NULL,
   PRIMARY KEY (`Servicio_idCliente`, `Forma_de_Pago_idForma_de_Pago`),
@@ -244,21 +244,21 @@ CREATE TABLE IF NOT EXISTS `test1`.`Servicio_has_Forma_de_Pago` (
   INDEX `fk_Servicio_has_Forma_de_Pago_Servicio1_idx` (`Servicio_idCliente` ASC),
   CONSTRAINT `fk_Servicio_has_Forma_de_Pago_Servicio1`
     FOREIGN KEY (`Servicio_idCliente`)
-    REFERENCES `test1`.`Servicio` (`idCliente`)
+    REFERENCES `sql238823`.`Servicio` (`idCliente`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Servicio_has_Forma_de_Pago_Forma_de_Pago1`
     FOREIGN KEY (`Forma_de_Pago_idForma_de_Pago`)
-    REFERENCES `test1`.`Forma_de_Pago` (`idForma_de_Pago`)
+    REFERENCES `sql238823`.`Forma_de_Pago` (`idForma_de_Pago`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test1`.`Apellido_has_Persona`
+-- Table `sql238823`.`Apellido_has_Persona`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test1`.`Apellido_has_Persona` (
+CREATE TABLE IF NOT EXISTS `sql238823`.`Apellido_has_Persona` (
   `Apellido_idApellido` INT NOT NULL,
   `Persona_idPersona` INT NOT NULL,
   PRIMARY KEY (`Apellido_idApellido`, `Persona_idPersona`),
@@ -266,12 +266,12 @@ CREATE TABLE IF NOT EXISTS `test1`.`Apellido_has_Persona` (
   INDEX `fk_Apellido_has_Persona_Apellido1_idx` (`Apellido_idApellido` ASC),
   CONSTRAINT `fk_Apellido_has_Persona_Apellido1`
     FOREIGN KEY (`Apellido_idApellido`)
-    REFERENCES `test1`.`Apellido` (`idApellido`)
+    REFERENCES `sql238823`.`Apellido` (`idApellido`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Apellido_has_Persona_Persona1`
     FOREIGN KEY (`Persona_idPersona`)
-    REFERENCES `test1`.`Persona` (`idPersona`)
+    REFERENCES `sql238823`.`Persona` (`idPersona`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
